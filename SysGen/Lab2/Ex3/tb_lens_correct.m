a6e2b064c0e5a9ac4ea5ac9bab83c6577ef057b8
tghaefli <haefliger.p@posteo.de>
2016-04-29 20:00:51 +0200

    added lense correcture

--- SysGen/Lab2/Ex3/tb_lens_correct.m
+++ SysGen/Lab2/Ex3/tb_lens_correct.m
@@ -1,19 +1,35 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % DSVB RT-Lab
 % Lab2 Ex 3: SysGen model for correction of lens distortion effect.
+%       v1 : Standalone Model with dual-port ping-pong buffer
+%            Because of the buffer size required, this model can not 
+%            be used for the streaming environment with its original
+%            HDTV resolution.
+%            This version has only the "nearest neighbor" interpolation 
+%            method implemented. However, the architecture in principle 
+%            is prepared to add linear interpolation as well.
 % Waj, HSLU-T&A
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clear all; close all;
 
 %% HW-Model Parameters
 % Lens correction factor
-K = 0.000010;
+K = 0.00004;
+% Interpolation method for new row/col-index
+% 0 = nearest neighbor; 1 = linear interpolation
+IPM = 0;
+% Latency in # of cc
+LT_AdrGen = 1;
+LT_IdxCalc = 9;
+LT_IdxInpol = 3;
+LT_PingPong = 2;
+LT_PxlInpol = 1;
+% Resolution for distorted x/y index
+RES_DIST_XY = 4;
 
 %% Read in the test image (==> enable one of the lines  below <==)
-ImgIn = [0 30 80; 110 140 170;200 230 255];
-ImgIn = [255 255 255; 255 0 255;255 255 255];
-ImgIn = repmat([255 255 255 255 0 0 255 255 255 255]',10,100);
-%ImgIn = imread('im3dist.bmp'); %ImgIn = rgb2gray(ImgIn);
+ImgIn = repmat([255 255 255 255 0 0 255 255 255 255]',6,100);
+%ImgIn = imread('im3.bmp'); %ImgIn = rgb2gray(ImgIn);
 
 %% Calculate image constants
 SizeI = size(ImgIn);
@@ -27,16 +43,16 @@ Xcent = (ImgX-1)/2; % center of col index
 Xcorr = ones(ImgY,1)*([0:ImgX-1]-Xcent);  % difference of x from image center 
 Ycorr = ([0:ImgY-1]-Ycent)'*ones(1,ImgX); % difference of y from image center
 Radius = (Xcorr.*Xcorr+Ycorr.*Ycorr);
-Radius_ROM = Radius';                   % transpose for row-wise addressing
+Radius_ROM = Radius';                     % transpose for row-wise addressing
 
 %% Distort Image
 ImgDist=zeros(size(ImgIn));
 for y=1:ImgY
     for x=1:ImgX
-        t=1-K*Radius(x,y);
-        xd = ceil(((x-Xcent)*t)+Xcent);
-        yd = ceil(((y-Ycent)*t)+Ycent);
-        ImgDist(xd,yd) = ImgIn(x,y);
+        t=1-K*Radius(y,x);
+        xd = max(ceil(((x-Xcent)*t)+Xcent),1);
+        yd = max(ceil(((y-Ycent)*t)+Ycent),1);
+        ImgDist(yd,xd) = ImgIn(y,x);
     end
 end
 
